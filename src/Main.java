3794ef97f12566b46180bf8e3c6cf53343571d10
Lucas <lucasduartevieira0@gmail.com>
2023-07-06 23:50:01 -0300

    Finalização do game

--- src/Main.java
+++ src/Main.java
@@ -6,12 +6,6 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Jogo jogar = new Jogo();
-        Scanner scan = new Scanner(System.in);
-        System.out.println("1) jogar");
-        System.out.println("2) Regras");
-        String modo = scan.nextLine();
-        if(modo.equals("1")) {
-            jogar.Jogar();
-        }
+        jogar.Jogar();
     }
 }
